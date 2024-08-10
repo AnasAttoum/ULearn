8ec7a1682cff0311a2d274decec4b84143c01d21
Anas Attoum <anasattoum.12321@gmail.com>
2024-08-11 00:46:50 +0300

    Create ULearn Page

---
+++ javaScript/index.js
@@ -0,0 +1,73 @@
+const imgs = document.querySelectorAll('#partnersContainer>div')
+
+// ARROW LEFT FUNCTION  (PARTNERS SLIDER)
+const arrowLeft = document.getElementById('arrowLeft')
+
+const firstImage = document.querySelectorAll('#partners>div>div>div:first-child>img')
+console.log("🚀 ~ firstImage:", firstImage)
+const firstImageObserver = new IntersectionObserver(firstImage => {
+    firstImage.forEach(img => {
+        if (img.isIntersecting) {
+            arrowLeft.style.display='none'
+        }
+        else {
+            arrowLeft.style.display='block'
+        }
+    })
+}, { threshold: 1 })
+firstImage.forEach(element => firstImageObserver.observe(element));
+
+
+arrowLeft.addEventListener('click', () => {
+    imgs.forEach(img => {
+        img.style.transition += 'all .5s'
+        img.style.transform += 'translateX(150px)'
+    })
+})
+
+// ARROW RIGHT FUNCTION  (PARTNERS SLIDER)
+const arrowRight = document.getElementById('arrowRight')
+
+const lastImage = document.querySelectorAll('#partners>div>div>div:last-child>img')
+console.log("🚀 ~ lastImage:", lastImage)
+const lastImageObserver = new IntersectionObserver(lastImage => {
+    lastImage.forEach(img => {
+        if (img.isIntersecting) {
+            arrowRight.style.display='none'
+        }
+        else {
+            arrowRight.style.display='block'
+        }
+    })
+}, { threshold: 1 })
+lastImage.forEach(element => lastImageObserver.observe(element));
+
+
+arrowRight.addEventListener('click', () => {
+    imgs.forEach(img => {
+        img.style.transition += 'all .5s'
+        img.style.transform += 'translateX(-150px)'
+    })
+})
+
+
+
+
+
+// Add animation
+const intros = document.querySelectorAll('.intro>div');
+const observerBox = new IntersectionObserver(intros => {
+    intros.forEach(intro => {
+        if (intro.isIntersecting) intro.target.classList.add('simpleTranslateX');
+    })
+})
+intros.forEach(element => observerBox.observe(element));
+
+
+const ourTeamTitle= document.querySelectorAll('#ourTeamTitle');
+const observerTitle = new IntersectionObserver(titles => {
+    titles.forEach(title => {
+        if (title.isIntersecting) title.target.classList.add('simpleTranslateX');
+    })
+})
+ourTeamTitle.forEach(element => observerTitle.observe(element));
